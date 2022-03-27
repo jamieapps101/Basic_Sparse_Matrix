4b516eba4176c6072570493ac7bf580ee34a2324
jamie <jameiapps101@gmail.com>
2022-03-27 23:01:47 +0100

    debugging backward substitution with backward_substitution_test_1

--- src/lib.rs
+++ src/lib.rs
@@ -31,22 +31,18 @@ pub fn solve(a: Csr<f32>, b: Dense<f32>) -> Dense<f32> {
 /// forward substitution, solve Ly=b for y
 fn forward_substitution(l: Csr<f32>, b: Dense<f32>) -> Dense<f32> {
     let dims = b.get_dims();
-    // println!("dims: {dims:?}");
     let mut y = Dense::<f32>::new_default_with_dims(dims.cols, dims.rows);
     for col_index in 0..y.get_dims().cols {
-        // println!(">col_index={col_index}");
         for row_index in 0..y.get_dims().rows {
-            // println!("->row_index={row_index}");
             let b_element = b.get_col(col_index)[row_index];
-            let mut l_x: f32 = 0.0;
+            let mut l_x = 0.0;
             for entry in l.get_row_compact(row_index).unwrap() {
-                if row_index != entry.col_index {
-                    l_x = entry.v*y.get_col(col_index)[entry.col_index];
+                if entry.col_index != row_index {
+                    l_x = l_x + entry.v*y.get_col(col_index)[entry.col_index];
                 }
             }
-            let l_element = l.get_row_compact(row_index).unwrap().last().unwrap().v;
-            let y_element = (b_element-l_x)/l_element;
-            y.get_col_mut(col_index)[row_index] = y_element;
+            let next_y = (b_element - l_x)/l.get_row_compact(row_index).unwrap().last().unwrap().v;
+            y.get_col_mut(col_index)[row_index] = next_y;
         }
     }
     y
@@ -58,20 +54,14 @@ fn backward_substitution(l_star: Csr<f32>, y: Dense<f32>) -> Dense<f32> {
     let dims = y.get_dims();
     let mut x = Dense::<f32>::new_default_with_dims(dims.cols, dims.rows);
     for col_index in 0..x.get_dims().cols {
-        println!(">col_index={col_index}");
         for row_index in (0..x.get_dims().rows).rev() {
-            println!("->row_index={row_index}");
             let y_element = y.get_col(col_index)[row_index];
-
-            let mut l_star_x: f32 = 0.0;
-            for entry in l_star.get_row_compact(row_index).unwrap() {
-                if row_index != entry.col_index {
-                    l_star_x = entry.v*y.get_col(col_index)[entry.col_index];
-                }
+            let mut l_x = 0.0;
+            for entry in l_star.get_row_compact(row_index).unwrap().iter().skip(1) {
+                l_x = l_x + entry.v*x.get_col(col_index)[entry.col_index];
             }
-            let l_element = l_star.get_row_compact(row_index).unwrap().last().unwrap().v;
-            let x_element = (y_element-l_star_x)/l_element;
-            x.get_col_mut(col_index)[row_index] = x_element;
+            let next_x = (y_element - l_x)/l_star.get_row_compact(row_index).unwrap().last().unwrap().v;
+            x.get_col_mut(col_index)[row_index] = next_x;
         }
     }
     x
@@ -83,7 +73,7 @@ fn backward_substitution(l_star: Csr<f32>, y: Dense<f32>) -> Dense<f32> {
 mod test {
     use super::*;
     #[test]
-    fn forward_substitution_test() {
+    fn forward_substitution_test_0() {
         let b = Dense::<f32>::from_data(&[
             &[5.0,7.0,2.0],
         ]);
@@ -106,7 +96,30 @@ mod test {
     }
 
     #[test]
-    fn backward_substitution_test() {
+    fn forward_substitution_test_1() {
+        let b = Dense::<f32>::from_data(&[
+            &[7.0,3.0,1.0],
+        ]);
+
+        let l = Csr::<f32>::from_data(&[
+            &[5.0,0.0,0.0],
+            &[8.0,2.0,0.0],
+            &[3.0,7.0,1.0],
+        ]);
+
+        let y_ref = Dense::<f32>::from_data(&[
+            &[(7.0/5.0),-4.1,25.5],
+        ]);
+
+        let y = forward_substitution(l, b);
+
+        println!("y:\n{y:?}");
+
+        assert_eq!(y,y_ref)
+    }
+
+    #[test]
+    fn backward_substitution_test_0() {
         let y = Dense::<f32>::from_data(&[
             &[5.0,7.0,2.0],
         ]);
@@ -119,6 +132,29 @@ mod test {
 
         let x_ref = Dense::<f32>::from_data(&[
             &[-1.2857143, -1.0, -3.0],
+        ]);
+
+        let x = backward_substitution(l_star, y);
+
+        println!("x:\n{x:?}");
+
+        assert_eq!(x,x_ref)
+    }
+
+    #[test]
+    fn backward_substitution_test_1() {
+        let y = Dense::<f32>::from_data(&[
+            &[1.0,7.0,3.0],
+        ]);
+
+        let l_star = Csr::<f32>::from_data(&[
+            &[7.0,1.0,8.0],
+            &[0.0,2.0,3.0],
+            &[0.0,0.0,5.0],
+        ]);
+
+        let x_ref = Dense::<f32>::from_data(&[
+            &[(-32.0/35.0), 2.6, 0.6],
         ]);
 
         let x = backward_substitution(l_star, y);
