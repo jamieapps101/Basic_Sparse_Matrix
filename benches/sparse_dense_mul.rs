5f19e01aaa717a490fc93c2bcce9ac08d17eb4f6
jamie <jameiapps101@gmail.com>
2022-03-29 02:17:50 +0100

    updated benchmarking to sweep across different density matrices

--- benches/sparse_dense_mul.rs
+++ benches/sparse_dense_mul.rs
@@ -3,65 +3,70 @@ use criterion::*;
 
 use basic_sparse_matrix::{dense::Dense,sparse::Csr};
 
-fn bench_a_x(c: &mut Criterion) {
-    c.bench_function("iter", move |bencher| {
-        let mut a = Csr::<u32>::new(1000,1000);
-        let mut x = Dense::<u32>::new_default_with_dims(10,1000);
-
-        // assign random vars
-        let mut rng = rand::rngs::StdRng::seed_from_u64(1000);
-
-        for _ in 0..100000 {
-            let row = (rng.next_u64() % 1000) as usize;
-            let col = (rng.next_u64() % 1000) as usize;
-            let v = (rng.next_u64() %  255) as u32;
-            a.insert(v,row,col).unwrap();
-        }
-        let a = a.finalise();
-
-        for _ in 0..1000 {
-            let col = (rng.next_u64() % 10) as usize;
-            let row = (rng.next_u64() % 1000) as usize;
-            let v = (rng.next_u64() %  255) as u32;
-            x.get_col_mut(col)[row] = v;
-        }
-
-        bencher.iter_with_large_drop(|| {
-            let output = a.mul_dense(&x).unwrap();
-            assert_eq!(output.get_nnz(),63)
-        })
-    });
+fn sd_mul(c: &mut Criterion) {
+    let mut group = c.benchmark_group("sd_mul");
+    for i in 0..9 {
+        let density = (i+1)*10;
+        let elements = 10000*density;
+        group.throughput(Throughput::Elements(elements as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(elements), &elements, |b, &e| {
+            let mut a = Csr::<u32>::new((1000,1000));
+            let mut x = Dense::<u32>::new_default_with_dims(10,1000);
+            // assign random vars
+            let mut rng = rand::rngs::StdRng::seed_from_u64(1000);
+            for _ in 0..e {
+                let row = (rng.next_u64() % 1000) as usize;
+                let col = (rng.next_u64() % 1000) as usize;
+                let v = (rng.next_u64() %  255) as u32;
+                a.insert(v,row,col).unwrap();
+            }
+            let a = a.finalise();
+            for _ in 0..(e/100) {
+                let col = (rng.next_u64() % 10) as usize;
+                let row = (rng.next_u64() % 1000) as usize;
+                let v = (rng.next_u64() %  255) as u32;
+                x.get_col_mut(col)[row] = v;
+            }
+            b.iter(|| {
+                let _output = a.mul_dense(&x).unwrap();
+            });
+        });
+    }
 }
 
-fn bench_a_x_to_single(c: &mut Criterion) {
-    c.bench_function("iter", move |bencher| {
-        let mut a = Csr::<u32>::new(1000,1000);
-        let mut x = Dense::<u32>::new_default_with_dims(10,1000);
-
-        // assign random vars
-        let mut rng = rand::rngs::StdRng::seed_from_u64(1000);
-
-        for _ in 0..100000 {
-            let row = (rng.next_u64() % 1000) as usize;
-            let col = (rng.next_u64() % 1000) as usize;
-            let v = (rng.next_u64() %  255) as u32;
-            a.insert(v,row,col).unwrap();
-        }
-        let a = a.finalise();
-
-        for _ in 0..1000 {
-            let col = (rng.next_u64() % 10) as usize;
-            let row = (rng.next_u64() % 1000) as usize;
-            let v = (rng.next_u64() %  255) as u32;
-            x.get_col_mut(col)[row] = v;
-        }
-
-        bencher.iter_with_large_drop(|| {
-            let output = a.mul_dense(&x).unwrap();
-            assert_eq!(output.sum_elements(),1524672122);
-        })
-    });
+fn ss_add(c: &mut Criterion) {
+    let mut group = c.benchmark_group("ss_add");
+    for i in 0..9 {
+        let density = (i+1)*10;
+        let elements = 10000*density;
+        group.throughput(Throughput::Elements(elements as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(elements), &elements, |b, &e| {
+            let mut a = Csr::<u32>::new((1000,1000));
+            let mut x = Csr::<u32>::new((1000,1000));
+            // assign random vars
+            let mut rng = rand::rngs::StdRng::seed_from_u64(1000);
+            for _ in 0..e {
+                let row = (rng.next_u64() % 1000) as usize;
+                let col = (rng.next_u64() % 1000) as usize;
+                let v = (rng.next_u64() %  255) as u32;
+                a.insert(v,row,col).unwrap();
+            }
+            let a = a.finalise();
+            for _ in 0..e {
+                let row = (rng.next_u64() % 1000) as usize;
+                let col = (rng.next_u64() % 1000) as usize;
+                let v = (rng.next_u64() %  255) as u32;
+                x.insert(v,row,col).unwrap();
+            }
+            let x = x.finalise();
+            b.iter(|| {
+                let _output = a.add_sparse(&x).unwrap();
+            });
+        });
+    }
 }
 
-criterion_group!(benches, bench_a_x,bench_a_x_to_single);
+
+
+criterion_group!(benches, sd_mul, ss_add);
 criterion_main!(benches);
